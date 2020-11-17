68319cf15cbe6a522b1e6007bb696d55d9138481
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-18 01:27:44 +0700

    add error state and some tweaks.

--- app/src/main/java/com/cakrasuryainti/panther/ui/pages/report/panel/PanelReportMetaForm.kt
+++ app/src/main/java/com/cakrasuryainti/panther/ui/pages/report/panel/PanelReportMetaForm.kt
@@ -1,6 +1,5 @@
 package com.cakrasuryainti.panther.ui.pages.report.panel
 
-import android.content.res.ColorStateList
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
@@ -8,13 +7,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
@@ -25,7 +26,6 @@ import com.cakrasuryainti.panther.db.model.PanelReport
 import com.cakrasuryainti.panther.ui.RootViewModel
 import com.cakrasuryainti.panther.ui.theme.PantherTheme
 import com.google.android.material.chip.Chip
-import androidx.compose.runtime.livedata.observeAsState
 
 
 @Composable
@@ -53,9 +53,27 @@ fun MetaForm(
     report: PanelReport?,
     updateReport: (PanelReport) -> Unit
 ) {
+    var isDirty by remember { mutableStateOf(false) }
+
+    fun handleNext(report: PanelReport?) {
+        Log.d("test", report?.model.toString())
+        if (
+            report?.pekerjaan != "" &&
+            report?.panelName != "" &&
+            report?.model != "" &&
+            report?.serialNumber != "" &&
+            report?.location != ""
+        ) {
+            onNext()
+        } else {
+            isDirty = true
+        }
+    }
+
     fun updateState(report: PanelReport?) {
         if (report != null) updateReport(report)
     }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -66,61 +84,20 @@ fun MetaForm(
                     }
                 },
                 actions = {
-                    TextButton(onClick = { onNext() }) {
+                    TextButton(onClick = { handleNext(report) }) {
                         Text(
                             "Next",
                             color = Color.White,
-                            style = MaterialTheme.typography.button
                         )
                     }
                 }
             )
         })
     {
-        Column {
-            OutlinedTextField(
-                value = report?.pekerjaan ?: "",
-                onValueChange = {
-                    updateState(report?.copy(pekerjaan = it))
-                },
-                label = { Text("Pekerjaan") },
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
-            )
-            OutlinedTextField(
-                value = report?.panelName ?: "",
-                onValueChange = {
-                    updateState(report?.copy(panelName = it))
-                },
-                label = { Text("Panel") },
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
-            )
-            OutlinedTextField(
-                value = report?.model ?: "",
-                onValueChange = {
-                    updateState(report?.copy(model = it))
-                },
-                label = { Text("Type / Model") },
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
-            )
-            OutlinedTextField(
-                value = report?.serialNumber ?: "",
-                onValueChange = {
-                    updateState(report?.copy(serialNumber = it))
-                },
-                label = { Text("Serial Number") },
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
-            )
-            OutlinedTextField(
-                value = report?.location ?: "",
-                onValueChange = {
-                    updateState(report?.copy(location = it))
-                },
-                label = { Text("Lokasi") },
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
-            )
-
+        Column(modifier = Modifier.padding(8.dp)) {
             Surface(
-                modifier = Modifier.fillMaxWidth().padding(start = 8.dp, end = 8.dp, top = 16.dp),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, top = 16.dp, bottom = 8.dp),
                 border = BorderStroke(1.dp, MaterialTheme.colors.onBackground.copy(alpha = 0.4f)),
                 shape = RoundedCornerShape(8.dp)
             ) {
@@ -128,9 +105,8 @@ fun MetaForm(
                     modifier = Modifier.padding(8.dp)
                 ) {
                     Text(
-                        "Job Description",
+                        "Job Desc",
                         modifier = Modifier.padding(start = 8.dp, top = 8.dp),
-                        style = MaterialTheme.typography.body2
                     )
                     Row {
                         JobDesc.values().forEach { jobDesc ->
@@ -141,6 +117,7 @@ fun MetaForm(
                                         isCheckable = true
                                         setOnClickListener {
                                             updateState(report?.copy(jobDesc = jobDesc))
+                                            isChecked = true
                                         }
                                     }
                                 }
@@ -156,6 +133,59 @@ fun MetaForm(
                     }
                 }
             }
+            OutlinedTextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                value = report?.pekerjaan ?: "",
+                onValueChange = {
+                    updateState(report?.copy(pekerjaan = it))
+                },
+                label = { Text("Pekerjaan") },
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                isErrorValue = isDirty && report?.pekerjaan == "",
+            )
+            OutlinedTextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                value = report?.panelName ?: "",
+                onValueChange = {
+                    updateState(report?.copy(panelName = it))
+                },
+                label = { Text("Panel") },
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                isErrorValue = isDirty && report?.panelName == ""
+            )
+            OutlinedTextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                value = report?.model ?: "",
+                onValueChange = {
+                    updateState(report?.copy(model = it))
+                },
+                label = { Text("Type / Model") },
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                isErrorValue = isDirty && report?.model == ""
+            )
+            OutlinedTextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                value = report?.serialNumber ?: "",
+                onValueChange = {
+                    updateState(report?.copy(serialNumber = it))
+                },
+                label = { Text("Serial Number") },
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                isErrorValue = isDirty && report?.serialNumber == ""
+            )
+            OutlinedTextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                onImeActionPerformed = { imeAction, _ ->
+                    if (imeAction == ImeAction.Done) handleNext(report)
+                },
+                value = report?.location ?: "",
+                onValueChange = {
+                    updateState(report?.copy(location = it))
+                },
+                label = { Text("Lokasi") },
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                isErrorValue = isDirty && report?.location == ""
+            )
         }
     }
 }
@@ -163,7 +193,8 @@ fun MetaForm(
 @Preview
 @Composable
 fun FormPreview() {
+    var report by remember { mutableStateOf(PanelReport("")) }
     PantherTheme {
-        MetaForm(onNavigateBack = {}, onNext = {}, report = PanelReport(""), {})
+        MetaForm(onNavigateBack = {}, onNext = {}, report = report, updateReport = { report = it })
     }
 }
