0e343d41ce1d61d7ff3d8950878a7120ac5a20d7
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-20 01:23:28 +0700

    change some alignement.

--- app/src/main/java/com/cakrasuryainti/panther/ui/components/ReportImageListItem.kt
+++ app/src/main/java/com/cakrasuryainti/panther/ui/components/ReportImageListItem.kt
@@ -2,16 +2,19 @@ package com.cakrasuryainti.panther.ui.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawOpacity
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.cakrasuryainti.panther.db.model.ReportImage
 import com.cakrasuryainti.panther.ui.theme.PantherTheme
+import com.itextpdf.layout.property.VerticalAlignment
 import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
@@ -30,9 +33,48 @@ fun ReportImageListItem(image: ReportImage, modifier: Modifier = Modifier) {
                 contentScale = ContentScale.Crop,
             )
         }
-        Column {
-            Text("Deskripsi", modifier = Modifier.padding(start = 8.dp))
-            Text(image.description, modifier = Modifier.padding(start = 8.dp))
+        ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+            val (text, edit) = createRefs()
+            if (image.description != "") {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.constrainAs(text) {
+                        start.linkTo(parent.start, 16.dp)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(edit.start)
+                        height = Dimension.fillToConstraints
+                        width = Dimension.fillToConstraints
+                    }
+                ) {
+                    Text(image.description)
+                }
+            } else {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.constrainAs(text) {
+                        start.linkTo(parent.start, 16.dp)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(edit.start)
+                        height = Dimension.fillToConstraints
+                        width = Dimension.fillToConstraints
+                    },
+                ) {
+                    Text(
+                        "Belum ada deskripsi",
+                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+                    )
+                }
+            }
+            IconButton(
+                onClick = {},
+                modifier = Modifier.constrainAs(edit) {
+                    end.linkTo(parent.end)
+                }
+            ) {
+                Icon(Icons.Rounded.Edit, modifier = Modifier.drawOpacity(0.7f))
+            }
         }
     }
 }
@@ -45,6 +87,23 @@ private fun ReportImagePreview() {
             ReportImageListItem(
                 ReportImage(
                     description = "some description",
+                    reportId = "test-image.jpg",
+                    filePath = "test-image.jpg",
+                    id = ""
+                ),
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ReportImageWithoutDescriptionPreview() {
+    PantherTheme {
+        Surface(modifier = Modifier.padding(8.dp)) {
+            ReportImageListItem(
+                ReportImage(
                     reportId = "test-image.jpg",
                     filePath = "test-image.jpg",
                     id = ""
