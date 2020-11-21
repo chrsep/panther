a1718d0de0ce453db4e5bfb840a94e2c60355ec2
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-21 19:01:14 +0700

    remove unused file.

--- app/src/main/java/com/cakrasuryainti/panther/ui/Root.kt
+++ app/src/main/java/com/cakrasuryainti/panther/ui/Root.kt
@@ -10,7 +10,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import com.cakrasuryainti.panther.ui.pages.HomeContainer
-import com.cakrasuryainti.panther.ui.pages.report.CreateReport
 import com.cakrasuryainti.panther.ui.pages.report.HomeViewModel
 import com.cakrasuryainti.panther.ui.pages.report.panel.*
 import com.cakrasuryainti.panther.ui.theme.PantherTheme
@@ -27,7 +26,6 @@ fun Root() {
         // A surface container using the 'background' color from the theme
         NavHost(navController, startDestination = "home") {
             composable("home") { HomeContainer(navController, homeViewModel) }
-            composable("create") { CreateReport(navController) }
 
             composable("create/panel") { PanelReportMetaForm(navController, panelViewModel) }
             composable("create/panel/measurements") {
