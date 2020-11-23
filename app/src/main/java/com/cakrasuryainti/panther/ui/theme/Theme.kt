d0e8311cb02534ace4cad684aed450a92f571c7d
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-23 14:20:44 +0700

    change color to gray.

--- app/src/main/java/com/cakrasuryainti/panther/ui/theme/Theme.kt
+++ app/src/main/java/com/cakrasuryainti/panther/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = purple200,
@@ -13,7 +14,7 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = purple500,
+    primary = Color.DarkGray,
     primaryVariant = purple700,
     secondary = teal200
 
